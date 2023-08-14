375c0dfa2653ecc8327322983ed6ee8497ebb91f
arpit2222 <arpit2018cs164@gmail.com>
2023-08-14 21:06:11 +0530

    understanding code

--- routes/SurveyRoute.js
+++ routes/SurveyRoute.js
@@ -4,37 +4,37 @@ const SurveyController = require('../controllers/SurveyController');
 const checkToken = require('../middleware/auth');
 
 // Create a new survey
-router.post('/', checkToken, SurveyController.createSurvey);
+router.post('/', SurveyController.createSurvey);
 
 // Get a list of all surveys
-router.get('/', checkToken, SurveyController.getSurveys);
+router.get('/', SurveyController.getSurveys);
 
 // Get all surveys created by a specific client
 router.get('/client/',checkToken,  SurveyController.getSurveysByclient);
 
 // Get a single survey by ID
-router.get('/:surveyId', checkToken, SurveyController.findOne);
+router.get('/:surveyId', SurveyController.findOne);
 
 // Update a survey by ID
 router.put('/:surveyId',  SurveyController.update);
 
 // Delete a survey by ID
-router.delete('/:surveyId', checkToken, SurveyController.delete);
+router.delete('/:surveyId', SurveyController.delete);
 
 // delete all survey
-router.delete('/', checkToken, SurveyController.deleteAllSurveys);
+router.delete('/', SurveyController.deleteAllSurveys);
 
 //delete particular client survey
 router.delete('/client/removeS',  SurveyController.deleteSurvey );
 
 // Get expired surveys
-router.get('/expired/data', checkToken, SurveyController.getExpiredSurveys);
+router.get('/expired/data', SurveyController.getExpiredSurveys);
 
 //Get expired survey by Id
-router.get("/expired/:id", checkToken, SurveyController.getExpiredSurveyById);
+router.get("/expired/:id", SurveyController.getExpiredSurveyById);
 
 //Get expierdsurveys of a particular client
-router.get('/expired/client/c', checkToken, SurveyController.getExpiredSurveysByclientid);
+router.get('/expired/client/c', SurveyController.getExpiredSurveysByclientid);
 
 
 //delete expired survey of particular client
@@ -42,7 +42,7 @@ router.get('/expired/client/c', checkToken, SurveyController.getExpiredSurveysBy
 router.delete("/expired/removeS", SurveyController.deleteExpiredSurveys);
 
 // Get analysis of the most chosen answer for each question
-router.get('/:surveyId/analysis', checkToken, SurveyController.getSurveyAnalysis);
+router.get('/:surveyId/analysis', SurveyController.getSurveyAnalysis);
 
 //active and inactive the survey mainly for admin
 router.put('/:id/status', SurveyController.updateStatusSurvey);
