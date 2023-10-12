14edb04f1640d0becc8e439ee395c34d1dceeb72
moayaan1911 <ayaangames@gmail.com>
2023-10-12 15:47:47 +0530

    ad model done gcp post

--- models/AdsModel.js
+++ models/AdsModel.js
@@ -1,37 +1,40 @@
+/** @format */
+
 const mongoose = require('mongoose');
 
 const adSchema = new mongoose.Schema({
-    clientId: {
-        type:  mongoose.Schema.Types.ObjectId,
-        ref: 'Client',
-        required: true
-    },
-    sessionId: Number,
-    campaignName: String,
-    campaignType: String,
-    adName: String,
-    socialMediaPages: {},
-    startDate: String,
-    startTime: String,
-    endDate: String,
-    endTime: String,
-    audience: {
-        location: String,
-        ageFrom: Number,
-        ageTo: Number,
-        gender: String
-    },
-    image: String,
-    adContent: String,
-    adImpressions: Number,
-    staus: String,
-    tags: [],
-    assignedUsers: Number,
-    users: [],
-    status:{
-        type:String,
-        default:"unverified"
-    }
+  clientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Client',
+    required: true,
+  },
+  sessionId: Number,
+  campaignName: String,
+  adUrl: String,
+  campaignType: String,
+  adName: String,
+  socialMediaPages: {},
+  startDate: String,
+  startTime: String,
+  endDate: String,
+  endTime: String,
+  audience: {
+    location: String,
+    ageFrom: Number,
+    ageTo: Number,
+    gender: String,
+  },
+  image: String,
+  adContent: String,
+  adImpressions: Number,
+  staus: String,
+  tags: [],
+  assignedUsers: Number,
+  users: [],
+  status: {
+    type: String,
+    default: 'unverified',
+  },
 });
 
-module.exports = new mongoose.model("Ad", adSchema)
+module.exports = new mongoose.model('Ad', adSchema);
