726cb3e191f8385cfe1f9a639c8e1a9b8f5cff91
moayaan1911 <ayaangames@gmail.com>
2023-10-17 21:53:30 +0530

    profileImage

--- models/UsersModel.js
+++ models/UsersModel.js
@@ -1,17 +1,20 @@
+/** @format */
+
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  userId:  Number,
-  companyName:   { type: String, required: true },
-  companyType:   String, 
-  companyEmail:   { type: String, required: true },
-  companyAddress1:  String, 
-  companyAddress2:  String, 
-  status : { type: String, default: "unverified" },//unverified,verified,paused,stopped
-  walletAddress:  { type: String, required: true }, 
-  jwtSession:  String,
-  jwtExpire:  Date,
-  tags: []
+  userId: Number,
+  profileImage: { type: String, default: '' },
+  companyName: { type: String, required: true },
+  companyType: String,
+  companyEmail: { type: String, required: true },
+  companyAddress1: String,
+  companyAddress2: String,
+  status: { type: String, default: 'unverified' }, //unverified,verified,paused,stopped
+  walletAddress: { type: String, required: true },
+  jwtSession: String,
+  jwtExpire: Date,
+  tags: [],
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
