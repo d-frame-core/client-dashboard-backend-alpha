bdc673a32bc491ee40f3ab8fe847a63ea4a095cc
moayaan1911 <ayaangames@gmail.com>
2023-11-07 22:28:11 +0530

    backend updated

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const helmet = require("helmet");
-const path = require("path"); // Import the path module
+/** @format */
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const path = require('path'); // Import the path module
 
 // Convert relative paths to absolute paths
-const AdsRoute = require(path.join(__dirname, "routes", "AdsRoute"));
-const FaqRoute = require(path.join(__dirname, "routes", "FaqRoute"));
-const HelpRoute = require(path.join(__dirname, "routes", "HelpRoute"));
-const UsersRoute = require(path.join(__dirname, "routes", "UsersRoute"));
-const SurveyRoute = require(path.join(__dirname, "routes", "SurveyRoute"));
-const AuthRoute = require(path.join(__dirname, "routes", "AuthRoute"));
-const LearnMoreRoute = require(path.join(__dirname, "routes", "LearnMoreRoute"));
-const ProfileRoute = require(path.join(__dirname, "routes", "ImageRoute"));
-const BidsRoute = require(path.join(__dirname, "routes", "BidsRoute"));
-const AdminRoute = require(path.join(__dirname, "routes", "AdminRoute"));
-const DframeUser = require(path.join(__dirname, "routes", "DframeUser"));
+const AdsRoute = require(path.join(__dirname, 'routes', 'AdsRoute'));
+const FaqRoute = require(path.join(__dirname, 'routes', 'FaqRoute'));
+const HelpRoute = require(path.join(__dirname, 'routes', 'HelpRoute'));
+const UsersRoute = require(path.join(__dirname, 'routes', 'UsersRoute'));
+const SurveyRoute = require(path.join(__dirname, 'routes', 'SurveyRoute'));
+const AuthRoute = require(path.join(__dirname, 'routes', 'AuthRoute'));
+const LearnMoreRoute = require(path.join(
+  __dirname,
+  'routes',
+  'LearnMoreRoute'
+));
+const ProfileRoute = require(path.join(__dirname, 'routes', 'ImageRoute'));
+const BidsRoute = require(path.join(__dirname, 'routes', 'BidsRoute'));
+const AdminRoute = require(path.join(__dirname, 'routes', 'AdminRoute'));
+const DframeUser = require(path.join(__dirname, 'routes', 'DframeUser'));
 
-const cookieParser = require("cookie-parser");
-
+const cookieParser = require('cookie-parser');
+const { WalletRouter } = require('./routes/wallet.route');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -28,7 +34,7 @@ app.use(cookieParser());
 app.use(
   helmet({
     frameguard: {
-      action: "deny",
+      action: 'deny',
     },
     hidePoweredBy: true,
     xssFilter: true,
@@ -41,14 +47,14 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
   );
   next();
 });
@@ -58,13 +64,14 @@ app.use('/ads', AdsRoute);
 app.use('/users', UsersRoute);
 app.use('/dframeUser', DframeUser);
 app.use('/survey', SurveyRoute);
-app.use('/auth', AuthRoute); 
-app.use('/LearnMore', LearnMoreRoute);//admin done
-app.use('/Help', HelpRoute);//admin done
-app.use('/F&Q', FaqRoute);//admin done
-app.use("/bids", BidsRoute)
-app.use("/admin", AdminRoute)
+app.use('/auth', AuthRoute);
+app.use('/LearnMore', LearnMoreRoute); //admin done
+app.use('/Help', HelpRoute); //admin done
+app.use('/F&Q', FaqRoute); //admin done
+app.use('/bids', BidsRoute);
+app.use('/admin', AdminRoute);
 app.use('/uploads', express.static('uploads'), ProfileRoute);
+app.use('/wallet', WalletRouter);
 app.use('/', (req, res) => {
   res.status(200).send('App is live');
 });
